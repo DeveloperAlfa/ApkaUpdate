06975b84a388cdaaedc956bd2de8a1156c253097
krishnarai30 <krishna.rai30@yahoo.com>
2016-07-09 13:34:56 +0530

    jai shri ram.... 1. edited otp verify now cmplt 2. framework and arrays are done adapter created.. just population left.. ashish karde populate inhe

--- app/src/main/java/sd_dtu/apkaupdate/QueryActivity.java
+++ app/src/main/java/sd_dtu/apkaupdate/QueryActivity.java
@@ -2,12 +2,38 @@ package sd_dtu.apkaupdate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class QueryActivity extends AppCompatActivity {
 
+    Spinner districtsp,pstationsp,iosp;
+    EditText firnoet,otheriomob,customq;
+    Button submitqbtn;
+    ArrayAdapter<CharSequence> districtadapter;
+    ArrayAdapter<CharSequence> policestationadapter;
+    ArrayAdapter<CharSequence> ioadapter;
+    ArrayAdapter<CharSequence> queryapter;
+
+    public void adapterwork(){
+        districtadapter = ArrayAdapter.createFromResource(this, R.array.District, R.layout.support_simple_spinner_dropdown_item);
+        districtadapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        districtsp.setAdapter(districtadapter);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query);
+        districtsp = (Spinner) findViewById(R.id.districtspinner);
+        pstationsp = (Spinner) findViewById(R.id.police_station_spinner);
+        iosp=(Spinner)findViewById(R.id.iospinner);
+        firnoet = (EditText) findViewById(R.id.firno);
+        submitqbtn=(Button)findViewById(R.id.subqbtn);
+        otheriomob = (EditText) findViewById(R.id.other_io_mob);
+        customq = (EditText) findViewById(R.id.custom_query);
+        adapterwork();
+
     }
 }
--- app/src/main/java/sd_dtu/apkaupdate/VerifyActivity.java
+++ app/src/main/java/sd_dtu/apkaupdate/VerifyActivity.java
@@ -13,40 +13,19 @@ import android.widget.RadioButton;
 public class VerifyActivity extends AppCompatActivity {
 
     Button otpbtn;
-    EditText eemail,emob;
+    EditText emob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
-        eemail=(EditText)findViewById(R.id.Email);
         emob=(EditText)findViewById(R.id.Mobile);
-        emob.setEnabled(false);
 
         otpbtn=(Button)findViewById(R.id.rqstbtn);
 
     }
 
-    public void radCheck(View view){
-        boolean checked=((RadioButton)view).isChecked();
-        switch (view.getId())
-        {
-            case R.id.radioButton:
-                if(checked) {
-                    eemail.setEnabled(false);
-                    emob.setEnabled(true);
-                }
 
-                break;
-            case R.id.radioButton2:
-                if (checked) {
-                    emob.setEnabled(false);
-                    eemail.setEnabled(true);
-
-                }
-                break;
-        }
-    }
 
 
     public void onClick(View view){
@@ -63,18 +42,4 @@ public class VerifyActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.radioGroup) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
