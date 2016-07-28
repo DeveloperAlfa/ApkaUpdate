e793c52db9851b0346ff9f1a92e9b0434397ec1b
krishnarai30 <krishna.rai30@yahoo.com>
2016-07-29 01:28:17 +0530

    Corrected UI

--- app/src/main/java/sd_dtu/apkaupdate/DisplayDetails.java
+++ app/src/main/java/sd_dtu/apkaupdate/DisplayDetails.java
@@ -11,22 +11,12 @@ import android.widget.Toast;
  * Created by sam AR on 6/14/2016.
  */
 public class DisplayDetails extends AppCompatActivity {
-    TextView namec,datel,datec,arrest,progress,csstatus;
-    public void dispdetails(){
-        namec=(TextView) findViewById(R.id.namec);
-        datec=(TextView) findViewById(R.id.datec);
-        datel=(TextView) findViewById(R.id.datel);
-        arrest=(TextView) findViewById(R.id.arrest);
-        progress=(TextView) findViewById(R.id.progress);
-        csstatus=(TextView) findViewById(R.id.csstatus);
 
 
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_display_details);
-        dispdetails();
 
         Toast.makeText(this,"Complaint Button will start working here after 10 days.",Toast.LENGTH_LONG).show();
 
